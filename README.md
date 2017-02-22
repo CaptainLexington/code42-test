9556bc5decd445a7a9d11cc4326b83570cfe696c
Dale <dale@chrisdale.name>
2017-02-21 23:46:44 -0600

    Post Mortem

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
-To run this local demo, clone, `npm install`, and `npm start`
+To run this demo locally, clone, `npm install`, and `npm start`
--- src/App.js
+++ src/App.js
@@ -23,6 +23,21 @@ class About extends Component {
       <section className='about'>
         <h2>Thank You!</h2>
         <p>Thank you for giving me this opportunity to demonstrate my skills as a front-end developer.</p>
+        <p>This was my first time using some of these technologies.</p>
+        <ul>
+          <li><p><strong>React</strong>: Although I've worked on a few projects with React-derived ClojureScript libraries <a href='https://github.com/reagent-project/reagent'>reagent</a> and <a href='https://github.com/Day8/re-frame/'>re-frame</a>, I'd never worked with React in JavaScript before. Not only did I use JSX for the first time (not terribly painful!), I also had to learn a different system of data flow (<code>props</code> and <code>this.setState</code> are quite unlike how things work in re-frame). In order to minimize the workflow overhead, I used Facebook's drod-dead simple <a href='https://github.com/facebookincubator/create-react-app'>create-react-app</a> template to create the scaffolding and load up the dependencies. Thus, I have yet to get involved in the intricacies of babel or WebPack. I also had to learn to use react-router. I decided not to further complicate things by including redux (although I optimistically installed it as a dependency), even though I'm sure it provides a more elegant solution to the aforementioned data flow. There is so little data, however, I thought it easier to make do.</p></li>
+          <li><p><strong>fetch</strong>: Although I know in my heart of hearts that keeping jQuery around just for <code>$.ajax</code> is at best lazy and at worst perverse, I'm usually in environments that have jQuery around for legacy reasons anyway, so I've never forced myself to explore JavaScript alternatives to AJAX handling (although of course in ClojureScript there's a third way altogether). Fetch was surprisingly simple, although the apparently idiomatic (as per GitHub's docs) usage of piping processed data through successive <code>then</code>s surprised me and seems unnecessary.</p></li>
+        </ul>
+
+        <h4>Post Mortem</h4>
+        <p>I really enjoyed using React. I've long been a devotee of functional programming, and seek to use concepts like pure functions and immutability in everything I do. JavaScript doesn't always make it easy (although it easier than in <em>some</em> languages!), but React makes using them for UI updates, at least, feel completely natural, sane, and manageable.</p>
+        <p>That said, there were some things I found less than elegant—owing, no doubt, to my own ignorance. For instance, it was necessary for me to make GitHub API calls whenever the user changed. I ended up having to do this in both the <code>componentDidMount</code> and <code>componentWillGetProps</code> methods of both the <code>User</code> and <code>UserRepoList</code> components. Even factoring these calls out into a separate function and passing them the appropriate props, there remains an unpleasant redundancy. I expect there is a more satisying solution to be found, given that it seems like a common pattern, but I could not find it myself.</p>
+        <p>Additionally, I encountered a problem whereby my <code>User</code> component would sometimes (for instance, if refreshing the page while viewing a specific user) be required to render when the user in question was null (because the initial call to load the users hadn't completed). My initial instinct, to check in the <code>User render</code> method, proved rather ungainly as the app grew in complexity.</p>
+        <p>Despire these small drawbacks, I am very pleased with React and proud of my little experimental application. I hope you will agree it is nicely architechted, reasonably featureful, and not terrible to look at. I reiterate my gratitude for having had this opportunity, for even if we do not decide to proceed further, I have added a very useful skill to my arsenal.</p>
+
+        <p>Cheers,</p>
+        <p>C Warren Dale</p>
+
       </section>
     )
   }
@@ -91,7 +106,7 @@ class User extends Component {
   }
 
   componentDidMount () {
-    const user = this.props.users[this.props.params.id]
+    const user = this.props.user
     let component = this
     this.setState({
       user: user,
@@ -111,7 +126,7 @@ class User extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
-    const user = nextProps.users[nextProps.params.id]
+    const user = nextProps.user
     let component = this
     this.setState({
       user: user,
@@ -141,7 +156,7 @@ class User extends Component {
           <h2>{user.login}</h2>
           <Image alt={user.login} src={user.avatar_url} rounded height='100' />
           <p>{userData.location}</p>
-          <p>{userData.email}</p>
+          <a href={"mailto:" + userData.email}>{userData.email}</a>
           <p>Joined on {userData.created_at.substr(0, 10)}</p>
           <UserRepoList user={user} />
         </section>
@@ -187,10 +202,10 @@ class App extends Component {
             <Col xs={3} md={3}>
               <UserList users={this.state.users} />
             </Col>
-            <Col xs={9} md={9}>
+            <Col xs={9} md={6}>
               <Router history={hashHistory}>
                 <Route path='/' component={About} />
-                <Route path='/:id' component={(props) => (<User params={props.params} users={this.state.users} />)} />
+                <Route path='/:id' component={(props) => (<User params={props.params} user={this.state.users[props.params.id]} />)} />
               </Router>
             </Col>
           </Row>
